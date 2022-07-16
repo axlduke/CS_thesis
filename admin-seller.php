88cc8a692b038cd586422e222b29fee8e40e53ca
axlduke <63648856+axlduke@users.noreply.github.com>
2022-07-16 22:52:01 +0800

    updated

--- admin-seller.php
+++ admin-seller.php
@@ -114,6 +114,11 @@
                         </a>
                     </li>
                     <li class="mr-6 my-2 md:my-0">
+                        <a href="invoice.php" class="block py-1 md:py-3 pl-1 align-middle no-underline text-gray-400 hover:text-orange-400 border-b-2 border-white hover:border-orange-400">
+                            <i class="uil uil-receipt fa-fw mr-3 hover:text-orange-400"></i><span class="pb-1 md:pb-0 text-sm">Invoice</span>
+                        </a>
+                    </li>
+                    <li class="mr-6 my-2 md:my-0">
                         <a href="#_" class="block py-1 md:py-3 pl-1 align-middle text-gray-400 no-underline hover:text-orange-400 border-b-2 border-white hover:border-orange-400">
                             <i class="uil uil-comment-question fa-fw mr-3 hover:text-orange-400"></i><span class="pb-1 md:pb-0 text-sm">Support</span>
                         </a>
