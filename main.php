b6dbeb11efd98636a4a8cbb9e48a32b112ea6e25
AEArtemis <limpo.mark26@gmail.com>
2022-08-04 18:55:08 +0800

    carousel update

--- main.php
+++ main.php
@@ -43,7 +43,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="main.css">
     <script src="js/tab.js" defer></script>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />    
 
 </head>
 <body class="bg-gray-100 font-sans leading-normal tracking-normal">
@@ -237,14 +238,47 @@
             }
         </style>
         <div id="e-com" data-tab-content>
-            <!-- <div class="main-carousel">
-                <div class="cell"><img src="img/ace.png"></div>
-                <div class="cell"><img src="img/aldon.png"></div>
-                <div class="cell"><img src="img/diannne.png"></div>
-                <div class="cell"><img src="img/mark.png"></div>
-                <div class="cell"><img src="img/ivy.png"></div>
-            </div> -->
-
+        <!-- Carousel Start-->
+        <div class="owl-carousel owl-theme">
+        <div class="item"><img src="e-com/product_images/ace.png"></div>
+        <div class="item"><img src="e-com/product_images/mark.png"></div>
+        <div class="item"><img src="e-com/product_images/diannne.png"></div>    
+        <div class="item"><img src="e-com/product_images/aldon.png"></div> 
+        <div class="item"><img src="e-com/product_images/ace.png"></div> 
+        <div class="item"><img src="e-com/product_images/mark.png"></div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script type="text/javascript"> 
+        $('.owl-carousel').owlCarousel({    
+            loop:true,
+            items: 3,
+            autoWidth: false,
+            margin:0,
+            stagePadding: 0,
+            nav: false,
+            autoplay: true,
+            center: true,
+            touchDrag: true,
+            mouseDrag: true,
+            autoplayTimeout: 1000,
+            autoplayHoverPause: true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                600:{
+                    items:3,
+                },
+                1000:{
+                    items:5,
+                },
+                1400:{
+                    items:3,
+                }                
+            }
+        });                    
+        </script>
+        <!-- Carousel End--> 
             <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-2 px-10 mb-5">
             <?php     
             $products_posted="SELECT * from products ORDER BY rand()";
